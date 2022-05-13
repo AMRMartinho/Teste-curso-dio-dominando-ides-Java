80e071f41904b83ff1131e3fec8a061eabb85685
AMR Martinho <andremartinho1976@gmail.com>
2022-05-12 22:53:47 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,31 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+	
+		Gato gato = new Gato();
+		Livros Livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(Livros);
+		// TODO Auto-generated method stub
+		/*int a = 7;
+		  int b = 6;
+		         System.out.println("Hello World! " +(a+b));
+		         System.out.println("André Ramos");*/
+	}
+
+}
+class Livros {
+   private String nome;
+   private String npag;
+}
+
+
+
+
+
+
